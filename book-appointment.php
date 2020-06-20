52ee2af0dd5d5288950467c329a7ac49b15973e5
roppe <paponjak_91@hotmail.com>
2020-06-20 13:27:55 +0200

    images

--- book-appointment.php
+++ book-appointment.php
@@ -1,6 +1,6 @@
 <?php
 require_once "recaptchalib.php";
-$secret = "6LcTvVYUAAAAABOviJMksiZpUMmv_iik7hifd-ER";
+$secret = "#Secret Key goes here...";
 $response = null;
 $reCaptcha = new ReCaptcha($secret);
 if ($_POST["g-recaptcha-response"]) {
